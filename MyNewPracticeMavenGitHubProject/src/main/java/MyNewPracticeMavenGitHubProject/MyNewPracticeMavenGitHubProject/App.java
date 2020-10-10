9faa9f2b6ea2b7af757c4d2db46ed91640f3470e
vitth <vitth@LAPTOP-6HH84M3P>
2020-10-11 01:26:20 +0800

    first time commit of new project

---
+++ MyNewPracticeMavenGitHubProject/src/main/java/MyNewPracticeMavenGitHubProject/MyNewPracticeMavenGitHubProject/App.java
@@ -0,0 +1,13 @@
+package MyNewPracticeMavenGitHubProject.MyNewPracticeMavenGitHubProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
