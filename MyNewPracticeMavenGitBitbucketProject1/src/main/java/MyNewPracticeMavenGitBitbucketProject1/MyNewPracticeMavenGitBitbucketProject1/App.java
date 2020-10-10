8d09855462c7c3af1bcc8a2571c8d1caa87cf067
vitth <vitth@LAPTOP-6HH84M3P>
2020-10-11 02:19:17 +0800

    firsttimecommit for bitbucket repo

---
+++ MyNewPracticeMavenGitBitbucketProject1/src/main/java/MyNewPracticeMavenGitBitbucketProject1/MyNewPracticeMavenGitBitbucketProject1/App.java
@@ -0,0 +1,13 @@
+package MyNewPracticeMavenGitBitbucketProject1.MyNewPracticeMavenGitBitbucketProject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
