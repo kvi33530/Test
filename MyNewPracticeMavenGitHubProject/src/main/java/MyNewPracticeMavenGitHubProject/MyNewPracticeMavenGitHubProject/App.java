e0bc01a5539d8cc9f98ebc853219d9edd2d6b4b3
vitth <vitth@LAPTOP-6HH84M3P>
2020-10-11 13:09:12 +0800

    added new method

--- MyNewPracticeMavenGitHubProject/src/main/java/MyNewPracticeMavenGitHubProject/MyNewPracticeMavenGitHubProject/App.java
+++ MyNewPracticeMavenGitHubProject/src/main/java/MyNewPracticeMavenGitHubProject/MyNewPracticeMavenGitHubProject/App.java
@@ -11,5 +11,11 @@ public class App
         System.out.println( "Hello World!" );
         Newclass obj=new  Newclass();
         obj.newmethod();
+        App.new1();
+    }
+    
+    public static void new1()
+    {
+    	System.out.println("from App class");
     }
 }
